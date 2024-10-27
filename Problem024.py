fa7e48aa8307a3a632a836992ffe74b427375b06
adenm82 <159546421+adenm82@users.noreply.github.com>
2024-10-27 18:37:31 -0400

    Create Problem024.py

---
+++ Problem024.py
@@ -0,0 +1,18 @@
+res,sol=[],[]
+nums=[0,1,2,3,4,5,6,7,8,9]
+def backtrack(nums):
+    if len(sol)==10:
+        res.append(sol[:])
+    else:
+        if len(res)>1_000_000:
+            return
+        n=nums[:]
+        for x in n:
+            sol.append(x)
+            nums.remove(x)
+            backtrack(nums)
+            sol.pop()
+            nums.append(x)
+            nums.sort()
+backtrack(nums)
+print(res[999999])
