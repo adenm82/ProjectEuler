c0b85b0fa95e62bb2f7ad4db4ec08f7b956cfd0d
adenm82 <159546421+adenm82@users.noreply.github.com>
2024-10-30 13:05:34 -0400

    Create Problem130.py

---
+++ Problem130.py
@@ -0,0 +1,47 @@
+#Took like 20 minutes, might look for better solution
+import sys
+def gcd10(x):
+    nums=[2,5,10]
+    for y in nums:
+        if x%y==0:
+            return False
+    return True
+def sieve(num):
+    prime = [True for i in range(num+1)]
+    dict={}
+# boolean array
+    p = 2
+    while (p * p <= num):
+ 
+        # If prime[p] is not
+        # changed, then it is a prime
+        if (prime[p] == True):
+ 
+            # Updating all multiples of p
+            for i in range(p * p, num+1, p):
+                prime[i] = False
+                dict[i]=0
+        p += 1
+    return dict
+def R(k):
+    s=""
+    for x in range(k):
+        s+='1'
+    return int(s)
+def A(n):
+    k=1
+    while not R(k)%n==0:
+        k+=1
+    return k
+sys.set_int_max_str_digits(100_000)
+dict=sieve(100_000)
+comps=[x for x in range(2,100_000) if x in dict and gcd10(x)]
+sum=0
+c=[]
+for x in comps:
+    if (x-1)%(A(x))==0:
+        sum+=x
+        c.append(x)
+        if len(c)==25:
+            break
+print(sum)
